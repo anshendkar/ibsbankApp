1e8863674953c8640eb2d696f39973e7c68405ff
Adesh <adeshshendkar2001@gmail.com>
2025-04-07 20:41:57 +0530

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AccountOpeningComponent } from './components/account-opening/account-opening.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'/open-account' , component: AccountOpeningComponent},
+    {path:'', component: AccountOpeningComponent , pathMatch:'full'}
+
+];
---
+++ src/app/components/account-opening/account-opening.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-opening',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-opening.component.html',
+  styleUrl: './account-opening.component.scss'
+})
+export class AccountOpeningComponent {
+
+}
